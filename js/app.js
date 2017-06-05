138d6548a165ebf103abc5835d8ec9a747a0c870
Johanny LS <johanny.dls@gmail.com>
2017-06-05 03:01:22 -0300

    Primeiro commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var app = angular.module('appUFCG', ['ngStorage',
+									 'angularUtils.directives.dirPagination']);
+
+app.controller('ctrlUFCG', function ($scope, $http, $localStorage) {
+	
+});
